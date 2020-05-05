b3604b7e972d8004ba3f4b34b596b1a72ec2fee5
Naveen Raina <naveenraina@outlook.com>
2020-05-05 13:07:06 +0530

    add option on templates

--- src/components/ListTemplate.vue
+++ src/components/ListTemplate.vue
@@ -1,14 +1,30 @@
 <template>
     <div>
         <ul>
-            <li>template 1</li>
-            <li>template 2</li>
-            <li>template 3</li>
+            <li v-for="item in templates" @click="showDetail()" v-bind:key="item.id">{{item.title}}</li>
+
+            <li @click="add()">ADD</li>
         </ul>
     </div>
 </template>
 <script>
 export default {
     name: 'ListTemplate',
+    data(){
+        return {
+            templates: [
+                { id: 1, title: 'template 1' },
+                { id: 2, title: 'template 2' },
+            ]
+        }
+    },
+    methods: {
+        showDetail(){
+
+        },
+        add(){
+            this.templates.push({ title: 'template new' })
+        }
+    }
 }
 </script>
